b72ec062d589f276a818bd7ff0ecb747540412d0
Siddharth-Chandra <siddharthchandra98@gmail.com>
2023-01-28 14:22:49 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from "./About";
 import Courses from "./Courses";
 import Contact from "./Contact";
 import Navbar  from "./Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 
 
 const App=()=> 
@@ -15,12 +15,12 @@ const App=()=>
     return (
         <>
         <Navbar />
-
-        <Routes>
+        <Routes basename="/Piechart" >
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/about" element={<About/>}/>
             <Route exact path="/courses" element={<Courses/>}/>
             <Route exact path="/contact" element={<Contact/>}/>
+            <Route path='*' element={<Navigate to='/' />} />
      
        </Routes>
        </>
